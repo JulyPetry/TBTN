b90f2b38b1c5f0ddf77315f5fea4b5f8af7e315b
JulyPetry <who323@haw-hamburg.de>
2026-07-03 20:44:18 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -294,9 +294,8 @@ function initAsh() {
       i === 0 ? ctx.moveTo(px, py) : ctx.lineTo(px, py);
     }
     ctx.closePath();
-    ctx.strokeStyle = p.color + p.alpha + ')';
-    ctx.lineWidth = 0.9;
-    ctx.stroke();
+    ctx.fillStyle = p.color + p.alpha + ')';
+    ctx.fill();
     ctx.restore();
   }
 
